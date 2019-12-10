e143e5cc6cdf20c9d2c66c49d27f46b8f3569ebe
zhangsan <971735930@qq.com>
2019-12-10 15:31:26 +0800

    合并分支   加入注释2222

--- src/main/java/com/_520it/crm/domain/Department.java
+++ src/main/java/com/_520it/crm/domain/Department.java
@@ -7,13 +7,13 @@ import lombok.Setter;
 @Setter
 public class Department {
     private Long id;
-    //状态33333 -b1分支
+    //状态33333 -b1分支  合并
     private String sn;
-    //状态33333 -b1分支
+    //状态33333 -b1分支 合并
     private String name;
-    //状态33333 -b1分支
+    //状态33333 -b1分支 合并
     private Employee manager;
-    //状态33333 -b1分支
+    //状态33333 -b1分支 合并
     private Department parent;
     //状态33333 -b1分支
     private Boolean state;

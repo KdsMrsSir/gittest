dac913ac9abece953458f291d75803f89d91c08d
zhangsan <971735930@qq.com>
2019-12-10 15:18:16 +0800

    加入注释

--- src/main/java/com/_520it/crm/domain/Department.java
+++ src/main/java/com/_520it/crm/domain/Department.java
@@ -7,14 +7,16 @@ import lombok.Setter;
 @Setter
 public class Department {
     private Long id;
-
+    //状态
     private String sn;
-
+    //状态
     private String name;
-
+    //状态
     private Employee manager;
-
+    //状态
     private Department parent;
-
+    //状态
     private Boolean state;
+
+
 }

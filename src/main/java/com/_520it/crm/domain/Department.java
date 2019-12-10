5f0c849df6dc9855e73719238a5642c20c0afda2
zhangsan <971735930@qq.com>
2019-12-10 15:27:21 +0800

    b分支   加入注释2222

--- src/main/java/com/_520it/crm/domain/Department.java
+++ src/main/java/com/_520it/crm/domain/Department.java
@@ -7,15 +7,15 @@ import lombok.Setter;
 @Setter
 public class Department {
     private Long id;
-    //状态33333
+    //状态33333 -b1分支
     private String sn;
-    //状态33333
+    //状态33333 -b1分支
     private String name;
-    //状态33333
+    //状态33333 -b1分支
     private Employee manager;
-    //状态33333
+    //状态33333 -b1分支
     private Department parent;
-    //状态33333
+    //状态33333 -b1分支
     private Boolean state;
 
 
